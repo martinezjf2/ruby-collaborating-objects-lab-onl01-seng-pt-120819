6a58d0ed25a95f0e3dcdda9a647acdd8481d3adb
Jeffrey Martinez <martinezjf2@gmail.com>
2020-03-04 15:11:16 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,57 @@
+
+require "pry"
+
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    # @songs = []
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+
+  end
+
+  def print_songs #instance method iterates through @songs of an instance of Artist
+    # puts @songs.collect {|x| x.name}
+    self.songs.each {|song| puts song.name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.new(name)
+    end
+  end
+  
+  def self.find(name)     #class method detects instances from @@all class variable
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  # def self.create(name)   #class method creates & stores instances vs initializing
+  #   artist = Artist.new(name)
+  #   artist.save
+  #   artist
+  # end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+
+
+
+
+require "pry"
+
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+
+
+
+  def initialize(name)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(file)
+    # First step is to break up filename into artist, song, genre
+    # set a variable name song_name = new_name[1]
+    # set a variablt artist_name = new_artist_name = new_name[0]
+
+    # create a new artist with new_aritst
+    # create a new song with new_song_name
+    new_name = file.chomp(".mp3").split(" - ")
+    # binding.pry
+    new_song = Song.new(new_name[1])
+    new_song.artist_name = new_name[0]
+    # new_song.save
+    new_song
+  end
+  
+  # def add_song(name)
+  #   self.songs << song
+  # end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+end
